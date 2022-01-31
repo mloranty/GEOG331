96a77f2eb422e239607f2d398282506113119b16
Mike Loranty <mloranty@colgate.edu>
2022-01-31 12:16:45 -0500

    More activity 2 examples

--- activity2_script.R
+++ activity2_script.R
@@ -5,6 +5,13 @@
 # make a vector of tree heights
 heights <- c(30,41,20,22)
 
+#convert heights to cm 
+heights_cm <- heights*100
+
+
+#set up a matrix with 2 columns and fill in by rows
+#first argument is the vector of numbers to fill in the matrix
+Mat<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=TRUE)
 
 
 datW <- read.csv("Z:\\GEOG331_S22\data\noaa_weather\2011124.csv")
